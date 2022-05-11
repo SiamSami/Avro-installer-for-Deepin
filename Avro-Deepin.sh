4b96752cfbfccc96e5db10abc6f580b72b621211
SiamSami <justfortest16162215@gmail.com>
2022-05-11 11:32:25 +0600

    Update Avro-Deepin.sh

--- Avro-Deepin.sh
+++ Avro-Deepin.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install ibus ibus-m17n dconf-editor wget -y
+sudo apt install ibus ibus-m17n dconf-editor wget fonts-noto-core -y
 echo "export GTK_IM_MODULE=ibus" >> /$HOME/.bashrc
 echo "export XMODIFIERS=@im=ibus" >> /$HOME/.bashrc
 echo "export QT_IM_MODULE=ibus" >> /$HOME/.bashrc
@@ -12,9 +12,5 @@ sudo apt install ./avro_2.1-3_all.deb -y
 rm avro_2.1-3_all.deb
 dconf write /desktop/ibus/general/enable-by-default true
 dconf write /desktop/ibus/general/use-system-keyboard-layout true
-wget --no-check-certificate https://fahadahammed.com/extras/fonts/font.sh -O font.sh
-chmod +x font.sh
-bash font.sh
-rm font.sh
 im-config -n ibus 
 echo "Please restart your computer"
